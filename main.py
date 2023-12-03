57475c2d7adb9ff4e3cde25195bd61cbb9f269d7
Angelo <angelo94dollez@gmail.com>
2023-12-03 15:50:13 +0100

    tout les fichiers

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-with open('Nomination_Chirac1.txt','r')as fichier:
-    c=fichier.read()
-    print(c)
+with open('Nomination_Chirac1.txt','r')as chirac1:
+with open('Nomination_Chirac2.txt', 'r') as chirac2:
+with open('Nomination_Giscard dEstaing.txt', 'r') as giscard:
+with open('Nomination_Hollande.txt', 'r') as hollande:
+with open('Nomination_Macron.txt', 'r') as macron:
+with open('Nomination_Mitterrand1.txt', 'r') as mitte1:
+with open('Nomination_Mitterrand2.txt', 'r') as mitte2:
+with open('Nomination_Sarkozy.txt', 'r') as sarko:
