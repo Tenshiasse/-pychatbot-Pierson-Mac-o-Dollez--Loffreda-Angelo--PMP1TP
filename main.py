0cb0aee6a5859252d29808fe83f0b70504f0a62a
Angelo <angelo94dollez@gmail.com>
2023-12-03 16:33:38 +0100

    nv

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-with open('Nomination_Chirac1.txt','r')as chirac1:
-with open('Nomination_Chirac2.txt', 'r') as chirac2:
-with open('Nomination_Giscard dEstaing.txt', 'r') as giscard:
-with open('Nomination_Hollande.txt', 'r') as hollande:
-with open('Nomination_Macron.txt', 'r') as macron:
-with open('Nomination_Mitterrand1.txt', 'r') as mitte1:
-with open('Nomination_Mitterrand2.txt', 'r') as mitte2:
-with open('Nomination_Sarkozy.txt', 'r') as sarko:
+directory = "./speeches"
+a=list(directory,"txt")
+print(a)
