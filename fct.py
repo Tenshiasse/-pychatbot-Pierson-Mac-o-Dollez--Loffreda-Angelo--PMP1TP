f2ff122ed76bb65e0d47422a5e30dcdb277d7a4e
Miaoucéo <lalienestmauve@gmail.com>
2023-12-15 15:07:07 +0100

    à chier ne pas envoyer

--- fct.py
+++ fct.py
@@ -7,29 +7,20 @@ def list_of_files(directory, extension):
         files_names.append(filename)
  return files_names
 
-def clean(directory):
-    clean_directory = os.path.join("./", 'clean')
-    if not os.path.exists(clean_directory):
-        os.makedirs(clean_directory)
+def filemodif(files_names):
+    for i in range(len(files_names)):
+        f_name = files_names[i]
+        afiles = open(f_name, "w")
+        clean = afiles.read().lower()
+        clean = clean.replace("'"," ")
+        clean = clean.replace("!"," ")
+        clean = clean.replace(".", " ")
+        clean = clean.replace(",", " ")
+        clean = clean.replace(":", " ")
+        clean = clean.replace(";", " ")
+        clean = clean.replace("-", " ")
+        clean = clean.replace("?", " ")
 
-    fichiers = [f for f in os.listdir(directory) if f.endswith(".txt")]
 
-    for fichier in fichiers:
-        directory_entree = os.path.join(directory, fichier)
-        directory_sortie = os.path.join(clean_directory, fichier)
 
-        with open(directory_entree, "r", encoding="utf8") as entree:
-            lignes = entree.readlines()
-
-        liste = []
-        for ligne in lignes:
-            liste.append(str(ligne).replace("\n", " "))
-
-        liste = [ligne.replace("'", " ").replace("!", " ").replace(".", " ").replace(",", " ").replace(":", " ")
-                 .replace(";", " ").replace("-", " ").replace("?", " ").replace("  ", " ").replace("   ", " ")
-                 .replace("    ", " ").replace("     ", " ").lower() for ligne in liste]
-
-        with open(directory_sortie, "w", encoding="utf8") as sortie:
-            for ligne in liste:
-                sortie.write(ligne)
-
+    afiles.close()
